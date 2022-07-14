9297da38927b74f2d499daf77855d90dbf70a250
jon-aliu <71720933+jon-aliu@users.noreply.github.com>
2022-07-14 13:54:01 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -7,19 +7,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: " AIzaSyCvmVcBEMEN0KZy7dE79EqaA076b3LYHhg",
-
-  authDomain: "cryptodc-ed668.firebaseapp.com",
-
-  projectId: "cryptodc-ed668",
-
-  storageBucket: "cryptodc-ed668.appspot.com",
-
-  messagingSenderId: "61987501699",
-
-  appId: "1:61987501699:web:68260858a5b8daacd68db6",
-
-  measurementId: "G-BSR9GLSTL4",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 
