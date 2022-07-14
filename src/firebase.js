38ab9758ef41bb84e82c3dea27c5db87604cdeee
jon-aliu <71720933+jon-aliu@users.noreply.github.com>
2022-07-14 13:59:13 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -13,6 +13,7 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  
 };
 
 
